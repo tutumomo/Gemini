d8c521f11bfab08d5d450e758c3f06f204107189
TU. CHENG SHIN <tutumomo@gmail.com>
2023-12-26 00:55:38 +0800

    U

--- stream.py
+++ stream.py
@@ -5,7 +5,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("Write a short poem about a cat", stream=True)
+response = model.generate_content("寫一首跟貓有關的詩", stream=True)
 
 for chunk in response:
     print(chunk.text, end="", flush=True)
